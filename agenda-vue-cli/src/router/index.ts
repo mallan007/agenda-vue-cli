963ede64e0b5efab9d442b5b025ea04a41947be3
Allan Queiroz <allanqueiroz.eng@gmail.com>
2024-08-13 14:37:33 -0300

    (feature)[Frontend] Configuring routers

--- agenda-vue-cli/src/router/index.ts
+++ agenda-vue-cli/src/router/index.ts
@@ -10,12 +10,19 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/add',
+      name: 'add',
+      component: () => import('../views/AddView.vue')
+    },
+    {
+      path: '/details/:id',
+      name: 'details',
+      component: () => import('../views/DetailsView.vue')
+    },
+    {
+      path: '/update/:id',
+      name: 'update',
+      component: () => import('../views/UpdateView.vue')
     }
   ]
 })
